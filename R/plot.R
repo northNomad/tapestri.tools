7fa0d590e43d2b30c0ff0affd1fae7e18646fb44
northNomad <alexch.liu@mail.utoronto.ca>
2021-06-22 15:34:18 -0400

    add plot_protein_by_clones

--- R/plot.R
+++ R/plot.R
@@ -62,3 +62,101 @@ plot_upset_with_variants <- function(h5f,
     }
   }
 }
+
+
+
+
+
+#' Plot protein abundance across clones
+#'
+#' Uses ggplot framework to plot protein abundance across groups of cells (aka, clones).
+#'
+#' @param h5f h5f
+#' @param index_cells_list A named list of integers.
+#'   The names are the names of the clones.
+#'   The integers are the index of cells that belong to the corresponding clones.
+#' @param select_proteins A character vector. Specifies the proteins to plot.
+#' To see available options, run \code{rhdf5::h5read(h5f, "/assays/protein_read_counts/ca/id")}
+#' @param geom Either \code{ridges} or \code{boxplot}. Default is \code{ridges}.
+#' @return A named list of ggplot objects
+#' @examples
+#' #Compare CD34 expression between two clones.
+#' #clone1: IDH1_R132H single mutant
+#' #clone2: IDH1_R132H & NPM1c double mutant
+#'
+#'
+#' \dontrun{
+#' idh1_het <- get_cells_index(h5f, variants = c(IDH1_R132H = "chr2:209113112:C/T"), int_ngt = "1")
+#' npm1c <- get_cells_index(h5f, variants = c(NPM1c = "chr5:170837543:C/CTCTG"), int_ngt = "1")
+#' npm1_wt <- get_cells_index(h5f, variants = c(NPM1c = "chr5:170837543:C/CTCTG"), int_ngt = "0")
+#'
+#' index_clone1 <- intersect(idh1_het, npm1_wt)
+#' index_clone2 <- intersect(idh1_het, npm1c)
+#' index_cells_list <- list(clone1 = index_clone1, clone2 = index_clone2)
+#'
+#' plot_protein_by_clones(h5f, index_cells_list, select_proteins = "CD34", geom = "ridges")
+#' }
+plot_protein_by_clones <- function(h5f,
+                                   index_cells_list,
+                                   select_proteins = NULL,
+                                   geom = c("ridges", "boxplot")){
+
+  x <- read_protein_counts(h5f, transpose = TRUE, index_cells = NULL, normalization = "clr")
+
+  #Specifies protein
+  if(!is.null(select_proteins)){
+    #check if selected proteins are available
+    protein_check <- !(select_proteins %in% colnames(x))
+    if(any(protein_check)){stop("selected protein is not present in data")}
+
+    x <- x %>% as.data.frame() %>% dplyr::select(select_proteins)
+  }
+
+  ###
+  group_names <- names(index_cells_list)
+  dt <- data.frame()
+  for(i in group_names){
+    dt_sub <- x[index_cells_list[[i]], ] %>% as.data.frame()
+    dt_sub$group <- i
+
+    dt <- dplyr::bind_rows(dt, dt_sub)
+  }
+  ###
+
+  ###
+  #Make data tidy for plotting
+  dt <- dt %>%
+    pivot_longer(cols = 1:(ncol(.)-1),
+                 names_to = "protein",
+                 values_to = "norm_count")
+  ###
+
+  ###
+  #declare list to store plots
+  plot_list <- list()
+
+  #plot
+  if(geom == "ridges"){
+    for(i in select_proteins){
+      dt_sub <- subset(dt, protein == i)
+      p <- ggplot(dt_sub, aes(norm_count, group)) +
+        geom_density_ridges() +
+        ggtitle(i)
+
+      plot_list[[i]] <- p
+    }
+  }
+
+  if(geom == "boxplot"){
+    for(i in select_proteins){
+      dt_sub <- subset(dt, protein == i)
+      p <- ggplot(dt_sub, aes(group, norm_count)) +
+        geom_boxplot() +
+        ggtitle(i)
+
+      plot_list[[i]] <- p
+    }
+  }
+  ###
+  return(plot_list)
+}
