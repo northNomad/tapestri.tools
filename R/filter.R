0ce365268dc6274ff12ee9b25d1c4421d2be80ba
northNomad <alexch.liu@mail.utoronto.ca>
2023-05-26 14:39:01 -0400

    Fix count_cells

--- R/filter.R
+++ R/filter.R
@@ -32,22 +32,37 @@ count_cells <- function(h5f, variants){
 
   #count
   dt.ngt <- apply(dt.ngt, 1, table)
-
-  m <- dt.ngt %>% as.matrix() %>% t() %>% as.data.frame()
-  m[, 5] <- rownames(m)
-  m <- m[, c(5, 1, 2, 3, 4)]
   
-  colnames(m) <- c("Variant", "NGT0", "NGT1", "NGT2", "NGT3")
-  #where to place the counts in output matrix
-  # col_index_list <- lapply(dt.ngt, function(x) as.integer(unlist(dimnames(x))) + 1)
-
-  #initialize output matrix
-  # m <- matrix(ncol = 4, nrow = length(variants), dimnames = list(names(variants), paste0("NGT", 0:3)))
-
-  #write output
-  # for(i in 1:length(variants)){
-  #   m[i, col_index_list[[i]]] <- as.numeric(dt.ngt[[i]])
-  # }
+  #Check class (will be different if not all variants have values of 0, 1, 2, 3)
+  class <- class(dt.ngt)[1]
+  
+  #if matrix
+  if(class == "matrix"){
+    m <- dt.ngt %>% as.matrix() %>% t() %>% as.data.frame()
+    m[, 5] <- rownames(m)
+    m <- m[, c(5, 1, 2, 3, 4)]
+    
+    colnames(m) <- c("Variant", "NGT0", "NGT1", "NGT2", "NGT3")
+  }
+  
+  #if list
+  if(class == "list"){
+    #where to place the counts in output matrix
+    col_index_list <- lapply(dt.ngt, function(x) as.integer(unlist(dimnames(x))) + 1)
+    
+    #initialize output matrix
+    m <- matrix(ncol = 4, nrow = length(variants), dimnames = list(names(variants), paste0("NGT", 0:3)))
+    
+    #write output
+    for(i in 1:length(variants)){
+      m[i, col_index_list[[i]]] <- as.numeric(dt.ngt[[i]])
+    }
+    m <- as.data.frame(m)
+    m[, 5] <- rownames(m)
+    m <- m[, c(5, 1, 2, 3, 4)]
+    
+    colnames(m) <- c("Variant", "NGT0", "NGT1", "NGT2", "NGT3")
+  }
 
   #replace NA with zeros
   m[is.na(m)] <- 0
