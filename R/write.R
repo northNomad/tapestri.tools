e16645b8f799c6fbfd1dc5c8843a6de76cb854b7
AlexCH Liu <60832021+northNomad@users.noreply.github.com>
2023-08-10 15:30:26 -0400

    Add ParentVector_to_DiagrammeR

--- R/write.R
+++ R/write.R
@@ -35,6 +35,42 @@ write_scite_inputs <- function(h5f,
 
 
 
+#' Parse SCITE mutation tree in parent vector format into mermaid code
+#'
+#' A helper function to write mermaid code. Used for visualization with \code{DiagrammeR::grViz()}.
+#'
+#' @param ParentVector A (numeric) vector that uses zero-based indexing.
+#' Ideally you'd get this vector from the posterior *.samples* file from SCITE output.
+#' @param Variants A character vector containing the names of the variants.
+#' It should be in the same order as the rows in the matrix you used as input for SCITE.
+#' @return A string. Can be directly used for visualization with \code{DiagrammeR::grViz()}
+#' #' @references https://github.com/cbg-ethz/SCITE
+#' @references Kuipers J et al. Single-cell sequencing data reveals widespread recurrence and loss of mutational hits in the life histories of tumorus. Genome Research 2017; 27:1885-1894.
+ParentVector_to_DiagrammeR <- function(ParentVector, Variants){
+  ParentVector <- ParentVector[1:length(Variants)]
+  ParentVector <- as.numeric(ParentVector)
+  ParentVector <- ParentVector + 1 #SCITE uses zero indexing.
+  out <- "digraph G {\nnode [color=deeppink4, style=filled, fontcolor=white];\n"
+
+  for(i in 1:length(Variants)){
+    Child <- Variants[i]
+    if(ParentVector[i] > length(Variants)){
+      Parent <- "Root"
+    }else{
+      Parent <- Variants[ParentVector[i]]
+    }
+
+    Code <- paste0(Parent, " -> ", Child, ";", "\n")
+    out <- paste0(out, Code)
+  }
+  out <- paste0(out, "}")
+  return(out)
+}
+
+
+
+
+
 #' Write input files for ENSEMBL VEP
 #'
 #' A helper function to write standard input files for ENSEMBL VEP.
